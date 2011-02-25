2c8ddaee81896c09f6cc58f54ffebfa391b76f22
Alan Robertson <alanr@unix.sh>
2011-02-25 14:05:51 -0700

    Put in a bunch more improvements to the doxygen documentation. This will probably lead to doing this more in other class files as well... Sigh...

--- include/netio.h
+++ include/netio.h
@@ -31,24 +31,47 @@ struct _NetIO {
 	Frame*		_cryptframe;
 	Frame*		_compressframe;
 	gint		_maxpktsize;
-	gboolean	(*bindaddr)(NetIO*, const NetAddr*);	///< Bind this object to the given address
-	gint		(*getfd)(const NetIO* self);		///< Return file/socket descriptor
-	gsize		(*getmaxpktsize)(const NetIO* self);	///< Return maximum packet size
-	gsize		(*setmaxpktsize)(NetIO*, gsize);	///< Set maximum packet size
-	void		(*sendframesets)			///< Send a FrameSet list.
-								///< @pre must have non-NULL _signframe
-				(NetIO* self,			///<[in/out] 'this' object pointer
-				 const NetAddr* destaddr,	///<[in] destination address
-				 GSList* framesets)		///<[in] List of FrameSets to send
-				 ;
-	GSList*		(*recvframesets)(NetIO*, NetAddr** src);///< Receive a single datagram's framesets
-	SignFrame*	(*signframe)(NetIO*self);		///< return a copied SignFrame
-	Frame*		(*cryptframe)(NetIO*self);		///< return a copied encryption frame
-	Frame*		(*compressframe)(NetIO*self);		///< return a copied compression frame
-	void		(*set_signframe)(NetIO* self, SignFrame* sign);//< Set digital signature object
-	void		(*set_cryptframe)(NetIO* self, Frame* crypt);//< Set encryption object
-	void		(*set_compressframe)(NetIO* self, Frame* compress);//< Set compression object
-	void		(*finalize)(NetIO* self);		///< Finalize this NetIO object
+	gboolean	(*bindaddr)			///<[in] Bind this NetIO to the given address
+				(NetIO* self,		///<[in/out] Object to bind
+				 const NetAddr*);	///<[in] Address to bind it to
+	gint		(*getfd)			///<[in] Return file/socket descriptor
+				(const NetIO* self);	///<[in] 'this' Object
+	gsize		(*getmaxpktsize)		///< Return maximum packet size for this NetIO
+				(const NetIO* self);	///< 'this' object
+	gsize		(*setmaxpktsize)		///< Set maximum packet size
+				(NetIO*,		///< 'this' object
+				 gsize);		///< size to set max pkt size to
+	void		(*sendframesets)		///< Send a FrameSet list to a @ref NetIO
+							///< @pre must have non-NULL _signframe
+				(NetIO* self,		///<[in/out] 'this' object pointer
+				 const NetAddr* dest,	///<[in] destination address
+				 GSList* framesets)	///<[in] List of FrameSets to send
+						   ;	// ";" is here to work around a doxygen bug
+	GSList*		(*recvframesets)		///< Receive a single datagram's framesets
+							///<@return GSList of FrameSets from packet
+				(NetIO*,		///<[in/out] 'this' object
+				 NetAddr** src);	///[out] source address of return result
+	SignFrame*	(*signframe)			///< return a copied SignFrame for use in sending
+				(NetIO*self);		///<[in]
+	Frame*		(*cryptframe)			///< return a copied encryption frame for sending
+				(NetIO*self);		///<[in] 'this' object
+	Frame*		(*compressframe)		///< return a copied compression frame for sending
+				(NetIO*self)		///<[in] 'this' object
+						   ;	// ";" is here to work around a doxygen bug
+	void		(*set_signframe)		///< Set digital signature for this NetIO channel
+				(NetIO* self,		///<[in] 'this' object
+				 SignFrame* sign)	///<[in] signature @ref SignFrame
+						   ;	// ";" is here to work around a doxygen bug
+	void		(*set_cryptframe)		///< Set encryption object
+				(NetIO* self,		///<[in] 'this' object
+				 Frame* crypt)		///<[in] encryption @ref Frame
+						   ;	// ";" is here to work around a doxygen bug
+	void		(*set_compressframe)		///< Set compression object
+				(NetIO* self,		///<[in] 'this' object
+				 Frame* compress)	///<[in] compression @ref Frame
+						   ;	// ";" is here to work around a doxygen bug
+	void		(*finalize)			///< Finalize this NetIO object
+				(NetIO* self);		///<[in] 'this' object
 };
 NetIO*	netio_new(gsize objsize); // Don't call this directly! - this is an abstract class...
 ///@}
