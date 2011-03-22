b6d9c8b11abc41fd691bbf280b95ca4244522d34
Alan Robertson <alanr@unix.sh>
2011-03-22 14:07:36 -0600

    Added a new page for source license and availability. Changed a rule to get rid of a >/dev/null since I changed options to make it unnecessary.

---
+++ docfiles/Misc.c
@@ -0,0 +1,18 @@
+/**
+@page SourceAndLicense Source Availability and License Terms
+
+@section LicenseTerms License Terms
+The version of the source from which these documents were created is licensed under the GNU Lesser General Public License (LGPL) version 3
+or any later version at your option, excluding the provision allowing for relicensing under the GPL at your option.
+
+If there are any source files which are not marked in this fashion, it is a clerical error.
+Those files are also provided under these same terms.
+
+@section SourceLocation Where To Find The Current Project Source
+As of this writing, the source to the latest version of this software is stored under Mercurial
+and can be found at http://hg.linux-ha.org/.
+
+An online version of these documents that corresponds to this source can be found here:
+http://linux-ha.org/source-doc/assimilation/html/index.html
+
+*/
