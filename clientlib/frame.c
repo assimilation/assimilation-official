a22150af95c7d828b2019c70eadd80bd0a0367e8
Alan Robertson <alanr@unix.sh>
2012-01-21 10:09:32 -0700

    Changed the configcontext class to be more general - with hash tables for values Also put frame code back into frame.c, and configcontext class back into configcontext.c (oops!)

--- clientlib/frame.c
+++ clientlib/frame.c
@@ -1,86 +1,167 @@
 /**
  * @file
- * @brief Implements the @ref ConfigContext class.
- * @details This file provides a place to remember and pass configuration defaults around.
+ * @brief Implements the @ref ConfigContext class - providing configuration values for nanoprobe runtime
+ * @details Allows for the holding of configuration values of various kinds.
  *
  * @author &copy; 2012 - Alan Robertson <alanr@unix.sh>
  * @n
  * Licensed under the GNU Lesser General Public License (LGPL) version 3 or any later version at your option,
  * excluding the provision allowing for relicensing under the GPL at your option.
  */
-#include <configcontext.h>
+#include <frame.h>
+#include <generic_tlv_min.h>
 #include <memory.h>
+/**
+ *
+ */
+FSTATIC void _frame_default_finalize(Frame * self);
+FSTATIC gsize _frame_total_size(Frame* f);
+FSTATIC gboolean _frame_default_isvalid(const Frame *, gconstpointer,	gconstpointer);
+FSTATIC void _frame_setvalue(Frame *, gpointer, guint16, GDestroyNotify valnotify);
+FSTATIC void _frame_updatedata(Frame *, gpointer, gconstpointer, FrameSet*);
+FSTATIC void _frame_dump(const Frame *, const char * prefix);
+FSTATIC void _frame_ref(Frame *);
+FSTATIC void _frame_unref(Frame *);
 
-FSTATIC void	_configcontext_setmgmtaddr(ConfigContext*, NetAddr*);
-FSTATIC void	_configcontext_setsignframe(ConfigContext* self, SignFrame* signframe);
-FSTATIC void	_configcontext_finalize(ConfigContext* self);
+///@defgroup Frame Frame class
+/// Class for holding/storing binary blobs -  Base class for all the other Frame types.
+///@{
+///@ingroup C_Classes
 
-ConfigContext*
-configcontext_new(gsize objsize)	///< size of ConfigContext structure (or zero for min size)
-{
-	ConfigContext * newcontext = NULL;
-	unsigned char addrbody[] = CONFIG_DEFAULT_ADDR;
-
-	if (objsize < sizeof(ConfigContext)) {
-		objsize = sizeof(ConfigContext);
-	}
-	newcontext = MALLOCCLASS(ConfigContext, objsize);
-	memset(newcontext, 0x00, objsize);
-	newcontext->deadtime		= CONFIG_DEFAULT_DEADTIME;
-	newcontext->hbtime		= CONFIG_DEFAULT_HBTIME;
-	newcontext->warntime		= CONFIG_DEFAULT_WARNTIME;
-	newcontext->setsignframe =	_configcontext_setsignframe;
-	newcontext->setmgmtaddr	=	_configcontext_setmgmtaddr;
-	newcontext->_finalize	=	_configcontext_finalize;
-	if (NULL == newcontext) {
-		goto errout;
-	}
-	newcontext->collectivemgmtaddr = netaddr_new(0, 0, CONFIG_DEFAULT_ADDRTYPE, addrbody, sizeof(addrbody));
-	if (NULL == newcontext->collectivemgmtaddr) {
-		goto errout;
-	}
-	newcontext->signframe = signframe_new(CONFIG_DEFAULT_SIGNFRAME_TYPE, 0);
-	if (NULL == newcontext->signframe) {
-		goto errout;
-	}
-	return newcontext;
-errout:
-	if (newcontext) {
-		newcontext->_finalize(newcontext);
-		newcontext = NULL;
-	}
-	g_return_val_if_reached(NULL);
-}
-
+/// Finalize a Frame
 FSTATIC void
-_configcontext_setsignframe(ConfigContext* self, SignFrame* signframe)
+_frame_default_finalize(Frame * self) ///< Frame to finalize
 {
-	g_return_if_fail(signframe != NULL);
-	self->signframe->baseclass.unref(CASTTOCLASS(Frame, self->signframe));
-	signframe->baseclass.ref(CASTTOCLASS(Frame, signframe));
-	self->signframe = signframe;
+	if (self->value && self->valuefinalize) {
+		self->valuefinalize(self->value);
+	}
+	memset(self, 0x00, sizeof(Frame));
+	FREECLASSOBJ(self);
+}
+/// Finalize a Frame
+FSTATIC void
+_frame_default_valuefinalize(gpointer value) ///< Value to finalize
+{
+	if (value) {
+		FREE(value);
+	}
 }
 
-FSTATIC void
-_configcontext_setmgmtaddr(ConfigContext* self, NetAddr* addr)
+/// Return total space required to put this frame in a packet (marshalled size)
+FSTATIC gsize
+_frame_total_size(Frame* f)	///< Frame to return the marshalled size of
 {
-	g_return_if_fail(addr != NULL);
-	self->collectivemgmtaddr->unref(self->collectivemgmtaddr);
-	self->collectivemgmtaddr = addr;
-	addr->ref(addr);
+	g_return_val_if_fail(f != NULL, 0);
+	return (gsize)(FRAME_INITSIZE + f->length);
 }
 
-FSTATIC void
-_configcontext_finalize(ConfigContext* self)
+/// Default @ref Frame 'isvalid' member function (always returns TRUE)
+FSTATIC gboolean
+_frame_default_isvalid(const Frame * self,		///< Frame object ('this')
+		       gconstpointer tlvptr,	///< Pointer to the TLV for this Frame
+		       gconstpointer pktend)	///< Pointer to one byte past the end of the packet
 {
-	if (self->collectivemgmtaddr) {
-		self->collectivemgmtaddr->unref(self->collectivemgmtaddr);
-		self->collectivemgmtaddr = NULL;
-	}
-	if (self->signframe) {
-		self->signframe->baseclass.unref(CASTTOCLASS(Frame, self->signframe));
-		self->signframe = NULL;
-	}
-	memset(self, 0x00, sizeof(*self));
-	FREECLASSOBJ(self); self = NULL;
+	(void)self;
+	(void)tlvptr;
+	(void)pktend;
+	return TRUE;
 }
+
+/// 'setvalue' @ref Frame member function.
+FSTATIC void
+_frame_setvalue(Frame * self,			///< Frame object ('this')
+		gpointer value,			///< Value to save away
+		guint16	length,			///< Length of value
+		GDestroyNotify valnotify)	///< Value destructor.
+{
+	if (self->value && self->valuefinalize) {
+		self->valuefinalize(self);
+	}
+	self->value = value;
+	self->length = length;
+	self->valuefinalize = valnotify;
+}
+
+/// Update packet data ('updatedata') @ref Frame member function
+FSTATIC void
+_frame_updatedata(Frame * self,			///< Frame object ('this')
+		  gpointer tlvptr,		///< Where the Frame TLV is
+		  gconstpointer pktend,		///< End of packet
+		  FrameSet* fs)			///< Frameset to update (or not)
+{
+	(void)fs;
+	// set_generic_tlv_value does pretty exhaustive error checking.
+	set_generic_tlv_value(tlvptr, self->value, self->length, pktend);
+}
+FSTATIC void
+_frame_ref(Frame * self)
+{
+	self->refcount += 1;
+}
+FSTATIC void
+_frame_unref(Frame * self)
+{
+	g_return_if_fail(self->refcount >= 1);
+	self->refcount -= 1;
+	if (self->refcount == 0) {
+		self->_finalize(self);
+	}
+}
+
+/// Construct a new frame - allowing for "derived" frame types...
+/// This can be used directly for creating basic binary frames, or by derived classes.
+Frame*
+frame_new(guint16 frame_type,	///< TLV type of Frame
+	  gsize framesize)	///< size of frame structure (or zero for sizeof(Frame))
+{
+	Frame * newframe;
+	if (framesize < sizeof(Frame)) {
+		framesize = sizeof(Frame);
+	}
+	newframe = MALLOCCLASS(Frame, framesize);
+	if (newframe != NULL) {
+		newframe->type = frame_type;
+		newframe->length = 0;
+		newframe->value = NULL;
+		newframe->dataspace	= _frame_total_size;
+		newframe->_finalize	= _frame_default_finalize;
+		newframe->isvalid	= _frame_default_isvalid;
+		newframe->setvalue	= _frame_setvalue;
+		newframe->updatedata	= _frame_updatedata;
+		newframe->dump		= _frame_dump;
+		newframe->valuefinalize	= NULL;
+		newframe->refcount	= 1;
+		newframe->ref		= _frame_ref;
+		newframe->unref		= _frame_unref;
+	}
+	return newframe;
+}
+/// Given marshalled data corresponding to a Frame (basic binary frame), return that corresponding Frame
+/// In other words, un-marshall the data...
+Frame*
+frame_tlvconstructor(gconstpointer tlvstart,	///<[in] start of TLV for this Frame
+		     gconstpointer pktend)	///<[in] first invalid byte past 'tlvstart'
+{
+	guint16		frametype = get_generic_tlv_type(tlvstart, pktend);
+	guint16		framelength = get_generic_tlv_len(tlvstart, pktend);
+	const guint8*	framevalue = get_generic_tlv_value(tlvstart, pktend);
+	Frame *		ret = frame_new(frametype, 0);
+	g_return_val_if_fail(ret != NULL, NULL);
+
+	ret->length = framelength;
+	ret->setvalue(ret, g_memdup(framevalue, framelength), framelength, _frame_default_valuefinalize);
+	return ret;
+}
+/// Basic "dump a frame" member function - we use g_debug() for output.
+/// It would be nice for derived classes to override this as appropriate.
+void
+_frame_dump(const Frame * f,			///<[in] Frame being dumped
+	    const char * prefix)		///<[in] Prefix to put out before each line when dumping it
+{
+	g_debug("%s%s: type = %d, length = %d", 
+		prefix,
+	        proj_class_classname(f),
+		f->type,
+		f->length);
+}
+///@}
--- include/configcontext.h
+++ include/configcontext.h
@@ -26,14 +26,20 @@ typedef struct _ConfigContext ConfigContext;
 /// and is managed by our @ref ProjectClass system.
 /// It provides the analog of global variables for remembering configuration defaults, etc.
 struct _ConfigContext {
-	guint32		deadtime;
-	guint32		hbtime;
-	guint32		warntime;
+	int		_refcount;
+	void		(*ref)(ConfigContext*);
+	void		(*unref)(ConfigContext*);
+	void		(*_finalize)(ConfigContext*);
+	GHashTable*	_intvalues;
+	GHashTable*	_strvalues;
 	NetAddr*	collectivemgmtaddr;
 	SignFrame*	signframe;
 	void		(*setmgmtaddr)(ConfigContext*, NetAddr*);
 	void		(*setsignframe)(ConfigContext*, SignFrame*);
-	void		(*_finalize)(ConfigContext*);
+	gint		(*getintvalue)(ConfigContext*, const char *name);
+	void		(*setintvalue)(ConfigContext*, const char *name, gint value);
+	const char*	(*getstringvalue)(ConfigContext*, const char *name);
+	void		(*setstringvalue)(ConfigContext*, const char *name, const char *value);
 };
 WINEXPORT ConfigContext*	configcontext_new(gsize objsize);
 
@@ -43,5 +49,16 @@ WINEXPORT ConfigContext*	configcontext_new(gsize objsize);
 #define	CONFIG_DEFAULT_ADDR	{127,0,0,1}
 #define	CONFIG_DEFAULT_ADDRTYPE	ADDR_FAMILY_IPV4
 #define	CONFIG_DEFAULT_SIGNFRAME_TYPE	G_CHECKSUM_SHA256
+
+#define CONFIGINAME_DEADTIME	"deadtime"
+#define CONFIGINAME_WARNTIME	"warntime"
+#define CONFIGINAME_HBTIME	"hbtime"
+
+/// Default values for some (integer) configuration values
+#define	CONFIGINTDEFAULTS {					\
+	{CONFIGINAME_DEADTIME,	CONFIG_DEFAULT_DEADTIME},	\
+	{CONFIGINAME_WARNTIME,	CONFIG_DEFAULT_WARNTIME},	\
+	{CONFIGINAME_HBTIME,	CONFIG_DEFAULT_HBTIME},		\
+	}
 ///@}
 #endif /* _CONFIGCONTEXT_H */
