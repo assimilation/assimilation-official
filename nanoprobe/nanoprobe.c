c8f49ab46e9a61e95231b6e7811a1dd3900ba16c
Alan Robertson <alanr@unix.sh>
2012-04-21 11:30:32 -0600

    First version of nanoprobe main program - it compiles.  Not yet tested.

---
+++ nanoprobe/nanoprobe.c
@@ -0,0 +1,202 @@
+/**
+ * @file 
+ * @brief nanoprobe main program.
+ * Starts up a nanoprobe and does nanoprobie things - deferring most of the work to others.
+ *
+ *
+ *
+ * @author &copy; 2012 - Alan Robertson <alanr@unix.sh>
+ * @n
+ * Licensed under the GNU Lesser General Public License (LGPL) version 3 or any later version at your option,
+ * excluding the provision allowing for relicensing under the GPL at your option.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <projectcommon.h>
+#include <framesettypes.h>
+#include <frameset.h>
+#include <ctype.h>
+#include <netgsource.h>
+#include <netioudp.h>
+#include <netaddr.h>
+#include <authlistener.h>
+#include <signframe.h>
+#include <cryptframe.h>
+#include <compressframe.h>
+#include <intframe.h>
+#include <addrframe.h>
+#include <cstringframe.h>
+#include <frametypes.h>
+#include <nanoprobe.h>
+
+#define	DEFAULT_PORT	1984
+
+gint64		pktcount = 0;
+GMainLoop*	loop = NULL;
+NetIO*		nettransport;
+NetGSource*	netpkt;
+NetAddr*	destaddr;
+NetAddr*	anyaddr;
+int		heartbeatcount = 0;
+int		errcount = 0;
+int		pcapcount = 0;
+int		wirepktcount = 0;
+
+gboolean gotnetpkt(Listener*, FrameSet* fs, NetAddr* srcaddr);
+void got_heartbeat(HbListener* who);
+void got_heartbeat2(HbListener* who);
+
+void fakecma_startup(AuthListener*, FrameSet* fs, NetAddr*);
+
+
+/// Test routine called when an otherwise-unclaimed NetIO packet is received.
+gboolean
+gotnetpkt(Listener* l,		///<[in/out] Input GSource
+	  FrameSet* fs,		///<[in/out] @ref FrameSet "FrameSet"s received
+	  NetAddr* srcaddr	///<[in] Source address of this packet
+	  )
+{
+	(void)l; (void)srcaddr;
+	++wirepktcount;
+	switch(fs->fstype) {
+	case FRAMESETTYPE_HBDEAD:
+		g_message("CMA Received dead host notification (type %d) over the 'wire'."
+		,	  fs->fstype);
+		break;
+	case FRAMESETTYPE_SWDISCOVER:
+		g_message("CMA Received switch discovery data (type %d) over the 'wire'."
+		,	  fs->fstype);
+		break;
+	case FRAMESETTYPE_JSDISCOVERY:
+		g_message("CMA Received JSON discovery data (type %d) over the 'wire'."
+		,	  fs->fstype);
+		break;
+	default:
+		g_message("CMA Received a FrameSet of type %d over the 'wire'."
+		,	  fs->fstype);
+	}
+	//g_message("DUMPING packet received over 'wire':");
+	//frameset_dump(fs);
+	//g_message("END of packet received over 'wire':");
+	fs->unref(fs);
+	return TRUE;
+}
+
+/**
+ * Nanoprobe main program.
+ *
+ * It leaves most of the work of starting up the nanoprobe code to nano_start_full()
+ */
+int
+main(int argc, char **argv)
+{
+	const guint8	loopback[] = CONST_IPV6_LOOPBACK;
+	const guint8	anyadstring[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	guint16		testport = DEFAULT_PORT;
+	SignFrame*	signature = signframe_new(G_CHECKSUM_SHA256, 0);
+	Listener*	otherlistener;
+	ConfigContext*	config = configcontext_new(0);
+	PacketDecoder*	decoder = nano_packet_decoder();
+
+	/// @todo handle signals - SIGINT, SIGTERM, (SIGUSR1, SIGUSR2?).
+	/// @todo initialize from a setup file - initial IP address, port, debug - anything else?
+
+
+	(void)argc;
+	(void)argv;
+	g_log_set_fatal_mask (NULL, G_LOG_LEVEL_ERROR|G_LOG_LEVEL_CRITICAL);
+
+	if (!netio_is_dual_ipv4v6_stack()) {
+		g_warning("This OS DOES NOT support dual ipv4/v6 sockets - this may not work!!");
+	}
+
+	config->setframe(config, CONFIGNAME_OUTSIG, &signature->baseclass);
+
+	// Create a network transport object for normal UDP packets
+	nettransport = &(netioudp_new(0, config, decoder)->baseclass);
+	g_return_val_if_fail(NULL != nettransport, 2);
+
+
+	// Construct the NetAddr we'll talk to (i.e., ourselves) and listen from
+	destaddr =  netaddr_ipv6_new(loopback, testport);
+	g_return_val_if_fail(NULL != destaddr, 3);
+	config->setaddr(config, CONFIGNAME_CMAINIT, destaddr);
+
+	// Construct a NetAddr to bind to (i.e. ANY address)
+	anyaddr =  netaddr_ipv6_new(anyadstring, testport);
+	g_return_val_if_fail(NULL != destaddr, 5);
+
+	// Bind to ANY address (as noted above)
+	g_return_val_if_fail(nettransport->bindaddr(nettransport, anyaddr),16);
+        anyaddr->  baseclass.unref(anyaddr);
+	anyaddr = NULL;
+	//g_return_val_if_fail(nettransport->bindaddr(nettransport, destaddr),16);
+
+	// Connect up our network transport into the g_main_loop paradigm
+	// so we get dispatched when packets arrive
+	netpkt = netgsource_new(nettransport, NULL, G_PRIORITY_HIGH, FALSE, NULL, 0, NULL);
+
+	// Observe all unclaimed packets
+	otherlistener = listener_new(config, 0);
+	otherlistener->got_frameset = gotnetpkt;
+	netpkt->addListener(netpkt, 0, otherlistener);
+	otherlistener->associate(otherlistener,netpkt);
+
+	// Unref the "other" listener - netpkt et al holds references to it
+	otherlistener->baseclass.unref(otherlistener); otherlistener = NULL;
+
+	// Free signature frame
+	signature->baseclass.baseclass.unref(signature); signature = NULL;
+
+	// Free misc addresses
+        destaddr-> baseclass.unref(destaddr); destaddr = NULL;
+
+	nano_start_full("/home/alanr/monitor/src/discovery_agents/netconfig"
+	,	900, netpkt, config);
+
+	// Free config object
+	config->baseclass.unref(config); config = NULL;
+
+	loop = g_main_loop_new(g_main_context_default(), TRUE);
+
+	/********************************************************************
+	 *	Start up the main loop - run our test program...
+	 *	(the one pretending to be both the nanoprobe and the CMA)
+	 ********************************************************************/
+	g_main_loop_run(loop);
+
+	/********************************************************************
+	 *	We exited the main loop.  Shut things down.
+	 ********************************************************************/
+
+	nano_shutdown(TRUE);	// Tell it to shutdown and print stats
+	g_message("Count of 'other' pkts received:\t%d", wirepktcount);
+
+	nettransport->finalize(nettransport); nettransport = NULL;
+
+	// Main loop is over - shut everything down, free everything...
+	g_main_loop_unref(loop); loop=NULL;
+
+	// Unlink misc dispatcher - this should NOT be necessary...
+	netpkt->addListener(netpkt, 0, NULL);
+
+	g_source_unref(&netpkt->baseclass); netpkt = NULL;
+
+	// This guy needs to be last - or nearly so...
+	g_main_context_unref(g_main_context_default());
+
+	// At this point - nothing should show up - we should have freed everything
+	if (proj_class_live_object_count() > 0) {
+		proj_class_dump_live_objects();
+		g_warning("Too many objects (%d) alive at end of test.", 
+			proj_class_live_object_count());
+		++errcount;
+	}else{
+		g_message("No objects left alive.  Awesome!");
+	}
+        proj_class_finalize_sys(); /// Shut down object system to make valgrind happy :-D
+	return(errcount <= 127 ? errcount : 127);
+}
