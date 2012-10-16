16fc0f3f8423d4320ad5a68d599ae5100f31b6cd
Alan Robertson <alanr@unix.sh>
2012-10-16 08:17:25 -0600

    Fixed some assumptions about how the build and source trees relate to each other. Updated documentation.

--- testcode/grind.sh
+++ testcode/grind.sh
@@ -1,5 +1,6 @@
+HERE=$(dirname $0)
 REPCOUNT=20
 GEN=--gen-suppressions=all
 GEN="--gen-suppressions=no --num-callers=50 --read-var-info=yes"
 GEN="--gen-suppressions=no --num-callers=50"
-sudo valgrind -q --sim-hints=lax-ioctls --leak-check=full --show-reachable=yes --suppressions=../../src/testcode/valgrind-msgs.supp $GEN --error-exitcode=100 --trace-children=no --child-silent-after-fork=yes ./mainlooptest $REPCOUNT
+sudo valgrind -q --sim-hints=lax-ioctls --leak-check=full --show-reachable=yes --suppressions=$HERE/valgrind-msgs.supp $GEN --error-exitcode=100 --trace-children=no --child-silent-after-fork=yes ./mainlooptest $REPCOUNT
