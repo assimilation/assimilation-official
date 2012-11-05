a7dcd0c2f5811e239c87ae784e8d70f0566f2783
Alan Robertson <alanr@unix.sh>
2012-11-04 19:33:14 -0700

    Changed license to GPL and made sure every source file has license info in it.

--- include/netioudp.h
+++ include/netioudp.h
@@ -5,10 +5,22 @@
  * This is a subclass of the @ref NetIO class.
  *
  *
- * @author &copy; 2011 - Alan Robertson <alanr@unix.sh>
+ * This file is part of the Assimilation Project.
+ *
+ * @author Copyright &copy; 2011, 2012 - Alan Robertson <alanr@unix.sh>
  * @n
- * Licensed under the GNU Lesser General Public License (LGPL) version 3 or any later version at your option.
- * excluding the provision allowing for relicensing under the GPL at your option.
+ *  The Assimilation software is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The Assimilation software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the Assimilation Project software.  If not, see http://www.gnu.org/licenses/
  */
 
 #ifndef _NETIOUDP_H
