66383701abd6cc7f97269d97c5990bb4e0a65c3b
Alan Robertson <alanr@unix.sh>
2013-02-09 10:56:00 -0700

    Updated the documents to reflect our pending contributor agreement.

--- docfiles/Misc.c
+++ docfiles/Misc.c
@@ -30,6 +30,72 @@ If you do not have the <i>wget</i> command, you can save that URL using your fav
 An online version of these documents that corresponds to the latest version of the source can be found here:
 http://linux-ha.org/source-doc/assimilation/html/index.html
 
+@page Contributing Contributing to the Assimilation Monitoring Project
+Interested in contributing to the Assimilation Project?
+Then you've come to the right place!
+
+Here are the steps you'll need to take to become a contributor to the Assimilation project:
+- Join the Assimilation Mailing List
+- Get a copy of Mercurial from http://mercurial.selenic.com
+- Clone the repository at http://hg.linux-ha.org/%7Cexperimental/assimilation/
+- Sign the Assimilation Contributor Agreement as described below
+- Discuss your ideas and on the mailing list
+- Create a patch implementing your ideas following project standards
+- Send Mercurial patches of your contributions to the Assimilation mailing list
+- Discuss on the mailing list
+
+@section WhyAContributorAgreement Why A Contributor Agreement?
+Contributor agreements are provided for a few reasons:
+- Ensure the legal integrity of the project - so that everyone asserts that they're providing original code to the project.
+  This protects users and other contributors.
+- Create a single legal entity that could easily enforce terms of the licenses.
+- Ensure that no one is submitting code that they know to be covered by patents, and dealing with the cases where there might be such patents.
+- Provide for the ability to change licenses in the future.  Depending on the future legal landscape, it might prove desirable
+  to switch licenses at some time in the future.  In addition, it creates the possibility to offer supported commercial versions
+  of the project under a different license.
+
+@section ContributorAgreement The Assimilation Contributor Agreement
+The Assimilation project's contributor agreements are borrowed from the <a href="http://harmonyagreements.org">Harmony Project</a>
+agreements.  They provide an excellent <a href="http://harmonyagreements.org/guide.html">guide</a> to understanding
+these contributor agreements.
+
+Note that this agreement does not require you to assign your copyrights (like the FSF for example), but only to share your copyright rights with the project.
+<i>You do not lose any rights to your contribution by signing this contribution agreement</i>.
+
+PUT LINKS TO THE AGREEMENT PDFs here -- along with the md5 or sha1 checksums here.
+
+@section SigningContributorAgreement How to Provide a Signed Copy of the Contributor Agreement
+There are two ways to provide a signed copy of the agreement:
+@subsection PrintedAgreement Printed Agreement
+- Print off a copy of the relevant agreement (individual or entity)
+- Read it carefully and agree to its terms
+- Sign it
+- Scan the entire agreement in as a single PDF with a resolution of 200 pixels/inch or more.
+- Email a copy of it to <a href="mailto:AssimilationSystems@gmail.com">&lt;AssimilationSystems@gmail.com&gt; Assimilation Systems Limited</a>.
+@subsection PrintedAgreement Emailed Agreement
+This method is only available if you can digitally sign emails using a well-known public key.
+- Download and read the document carefully and agree to its terms
+- Send a digitally signed email described below to <a href="mailto:AssimilationSystems@gmail.com">&lt;AssimilationSystems@gmail.com&gt; Assimilation Systems Limited</a>.
+ - Attach the PDF of the agreement from the web site
+ - In the body of the email put these statements
+  <tt>
+  I have read and agree to the terms of the attached document for contributions to the Assimilation Monitoring Project
+  (represented by Assimilation Systems Limited).
+  
+  Submitting this digitally signed email to Assimilation Systems Limited constitutes my acceptance of the terms of the attached agreement
+  and I provide this email as a legally binding signature to this agreement.
+
+  <i>Your Name and email address</i>
+
+  <i>Today's date</i>
+  </tt>
+
+@section WhatIsAssimilationSystemsLimited What Is Assimilation Systems Limited?
+When you read the agreement, you'll notice that the Assimilation Project contributor agreements are with Assimilation Systems Limited,
+not with an individual.
+The project founder (Alan Robertson) created Assimilation Systems Limited as a perpetual legal entity to be the
+owner of the rights vested in the project.  It is a Limited Liability Company established and registered in Colorado, USA.
+
 */
 /**
 @page ContactInfo Contact Information 
