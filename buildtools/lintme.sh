685ee87a90b2a157b279f81fd39311ddd780eb33
Alan Robertson <alanr@unix.sh>
2013-06-03 19:04:57 -0600

    Added the possibility of parameters and fixed the permissions on lintme.sh

--- buildtools/lintme.sh
+++ buildtools/lintme.sh
@@ -1,6 +1,28 @@
+#
+#
+# This file is part of the Assimilation Project.
+#
+# Author: Alan Robertson <alanr@unix.sh>
+# Copyright (C) 2013 - Assimilation Systems Limited
+#
+#  The Assimilation software is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  The Assimilation software is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with the Assimilation Project software.  If not, see http://www.gnu.org/licenses/
+#
+#
+
 #AssimCclasses.py AssimCtypes.py cmadb.py cma.py dispatchtarget.py droneinfo.py frameinfo.py hbring.py __init__.py messagedispatcher.py obsolete_cma.py packetlistener.py
 #	AssimCtypes.py is generated
 #	__init__.py is empty
 #	obsolete_cma.py is obsolete
 LIST='AssimCclasses.py cmadb.py cma.py dispatchtarget.py droneinfo.py frameinfo.py hbring.py messagedispatcher.py packetlistener.py'
-pylint --rcfile pylint.cfg $LIST
+pylint "$@" --rcfile pylint.cfg $LIST
