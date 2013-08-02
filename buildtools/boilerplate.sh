0441a5334ed7f7190f2db3fcaf9d08129a686c87
Alan Robertson <alanr@unix.sh>
2013-08-02 15:12:10 -0600

    Updated the boilerplate for our source files.

--- buildtools/boilerplate.sh
+++ buildtools/boilerplate.sh
@@ -5,17 +5,20 @@
 # Author: Alan Robertson <alanr@unix.sh>
 # Copyright (C) 2013 - Assimilation Systems Limited
 #
-#  The Assimilation software is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
+# Free support is available from the Assimilation Project community - http://assimproj.org
+# Paid support is available from Assimilation Systems Limited - http://assimilationsystems.com
 #
-#  The Assimilation software is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+# The Assimilation software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with the Assimilation Project software.  If not, see http://www.gnu.org/licenses/
+# The Assimilation software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the Assimilation Project software.  If not, see http://www.gnu.org/licenses/
 #
 #
