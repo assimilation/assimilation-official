28eabb3ad3cb973d87dd7e5bd3c4b39571f3f542
Alan Robertson <alanr@unix.sh>
2013-09-20 16:37:00 -0600

    Got rid of the "show-reachable" valgrind option Need to eventually make that optional, but for now, that should make the tests a lot more likely to succeed on random OSes

--- testcode/grind.sh
+++ testcode/grind.sh
@@ -26,4 +26,6 @@ export G_MESSAGES_DEBUG G_SLICE
 GEN=--gen-suppressions=all
 GEN="--gen-suppressions=no --num-callers=50 --read-var-info=yes"
 GEN="--gen-suppressions=all --num-callers=50"
-sudo valgrind -q --sim-hints=lax-ioctls --leak-check=full --show-reachable=yes --suppressions=$HERE/valgrind-msgs.supp $GEN --error-exitcode=100 --trace-children=no --child-silent-after-fork=yes ./mainlooptest $REPCOUNT
+OPTS="--show-reachable=yes"
+
+sudo valgrind -q --sim-hints=lax-ioctls --leak-check=full --suppressions=$HERE/valgrind-msgs.supp $GEN --error-exitcode=100 --trace-children=no --child-silent-after-fork=yes ./mainlooptest $REPCOUNT
