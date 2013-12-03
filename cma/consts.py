ea74bbefafc7ccb5b237445d26c43b6e16a6ce63
Alan Robertson <alanr@unix.sh>
2013-12-03 13:57:06 -0700

    Added code to test the MonitorAction code - and have it pass ;-)

--- cma/consts.py
+++ cma/consts.py
@@ -66,7 +66,7 @@ class CMAconsts(object):
     REL_tcpservice  = 'tcpservice'  # NODE_tcpipport    ->  NODE_ipproc
     REL_ipphost     = 'ipphost'     # NODE_tcpipport    ->  NODE_drone
     REL_hosting     = 'hosting'     # NODE_drone        ->  NODE_ipproc
-    REL_monitoringon = 'monitoringon'# NODE_ipproc      ->  NODE_drone
+    REL_monitoring  = 'monitoring'  # NODE_monitoraction->  NODE_ipproc OR SystemNode
     REL_tcpclient   = 'tcpclient'   # NODE_ipproc       ->  NODE_tcpipport
     #                  RingMember_* # NODE_drone        ->  NODE_ring
     #                  RingNext_*   # NODE_drone        ->  NODE_drone
