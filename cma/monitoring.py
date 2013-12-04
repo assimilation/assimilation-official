436f56ddb2ff4907c9a1b62d08a18273a4cf6643
Alan Robertson <alanr@unix.sh>
2013-12-04 12:51:13 -0700

    Updated comments and fixed pylint messages

--- cma/monitoring.py
+++ cma/monitoring.py
@@ -126,6 +126,9 @@ class MonitorAction(GraphNode):
 
 #
 #
+# W0232: class w/o __init__ method
+# R0903: too few public methods
+# pylint: disable=R0903,W0232
 class MonitoringRule:
     '''Abstract base class for implementing monitoring rules
 
@@ -148,164 +151,14 @@ class MonitoringRule:
 
     For example, if the command name matches Java, then invoke the java rules.
 
-        If a java rule matches Neo4j, then return the Neo4j monitoring action
-            and so on...
+    If a java rule matches Neo4j, then return the Neo4j monitoring action
+        and so on...
 
-        Note that for Java, the monitoring rule is likely to match the _last_ argument
-            in the argument string...
-                arg[__last__]...  Maybe that would be a good choice??
+    Note that for Java, the monitoring rule is likely to match the _last_ argument
+        in the argument string...
+            arg[-1] - python-style
 
     If it matches python, then match it against the second argument (arg[1])
 
     '''
-    rulelist = []
-
-    @staticmethod
-    def __meta_keyattrs__():
-        'Return our key attributes in order of significance'
-        return ['designation', 'domain']
-
-    def __init__(self):
-        'Register this rule in the rule registry'
-        MonitoringRules.register(self)
-
-    def makerule(self, service, servicehost, monitorrunningon=None):
-        raise NotImplementedError('abstract class (MonitoringRules)')
-        return False
-
-
-    def construct_mon_json(self, monitorclass, monitortype, monitorname, interval, timeout, provider=None, arglist=None):
-        '''Add
-          Parameters
-          ----------
-          monitorclass : str
-                The class of monitoring agent being invoked
-          monitortype : str
-                Which particular agent of class 'class' is being invoked to monitor the resource
-          monitorname : str
-                The name of this instance of resource being monitored
-          interval : int
-                The number of seconds between monitor operations
-          timeout : int
-                The maximum number of seconds to wait before declaring the monitor failed
-          provider : str, optional
-                The provider name (required for OCF agents, ignored otherwise).
-          arglist : dict-like, optional
-                A list of name/value pairs to be given to the resource agent
-                Currently only supported by OCF agents.
-
-          Returns
-          ----------
-          pyConfigContext object (whose __str__ method returns JSON).
-          '''
-        if arglist is None:
-            arglist_str = ''
-        else:
-            arglist_str = ', "%s": %s' % (REQENVIRONNAMEFIELD, str(pyConfigContext(init=arglist)))
-
-        if provider is None:
-            provider_str = ''
-        else:
-            provider_str = ', "%s":"%s"' % (REQPROVIDERNAMEFIELD, provider)
-
-
-        json = '{"%s":"%s", "%s":"%s", "%s":"%s", "%s":%d "%s":%d%s%s}' % (
-        REQCLASSNAMEFIELD, monitorclass,
-        REQTYPENAMEFIELD, monitortype,
-        REQRSCNAMEFIELD, monitorname,
-        REQREPEATNAMEFIELD, interval,
-        REQTIMEOUTNAMEFIELD, timeout,
-        provider_str, arglist_str)
-        return pyConfigContext(init=json)
-        
-        
-
-    @staticmethod
-    def createamonitoringrule(cls, service, monitorclass, monitortype, monitorname
-    ,           interval, timeout, monitorrunningon, arglist=None, provider=None):
-        '''Add rules for the requested kind of monitoring method to the graph,
-        and give it to the nanoprobe.
-        We have several steps to perform here:
-          1) Create monitor-action node in the graph
-          2) Relate the monitor-action node to the service
-          3) Relate the monitor-action node to the monitorrunningon node
-          4) Send the repeating monitor action to the monitorrunningon Drone
-
-          Parameters
-          ----------
-          cls : Class
-                The class which this node should be created for if it does not exist
-          service : GraphNode
-                The graph node which represents the entity to be monitored
-          monitorclass : str
-                The class of monitoring agent being invoked
-          monitortype : str
-                Which particular agent of class 'class' is being invoked to monitor the resource
-          monitorname : str
-                The name of this monitoring action
-          interval : int
-                The number of seconds between monitor operations
-          timeout : int
-                The maximum number of seconds to wait before declaring the monitor failed
-          monitorrunningon : Drone
-                The Drone graph node which runs this particular agent
-          arglist : dict-like, optional
-                A list of name/value pairs to be given to the resource agent
-          provider : str, optional
-                The provider name (required for OCF agents, ignored otherwise).
-
-          Returns
-          ----------
-          None
-        '''
-        pass
-
-
-    @staticmethod
-    def register(ruleobj):
-        rulelist.append(ruleobj)
-
-    @staticmethod
-    def makearule(service, servicehost, monitorrunningon=None):
-        if monitoringrunningon is None:
-            monitoringrunningon=servicehost
-        for rule in rulelist:
-            if rule.makerule(service, servicehost, monitorrunningon):
-                return True
-        return False
-
-class MonitorLSB(MonitoringRules):
-    '''Class for automatically creating monitoring rules for LSB services'''
-
-    def __init__(self, servicename, exepattern, interval, timeout
-        ,   casesensitive=True, monclass='lsb'):
-        '''If the exe for the service being monitored matches our regex, then we add a rule
-        for monitoring it using the status operation of the service.
-        This kind of monitoring only works <i>on</i> the machine where the service is running
-        - not over the network.
-        '''
-        self.servicename=servicename
-        self.monclass=monclass
-        flags = 0
-        if not casesensitive:
-            flags=re.IGNORECASE
-        self.regex = re.compile(exepattern, flags)
-        self.servicename = servicename
-        MonitoringRules.__init__(self)
-
-    def makerule(self, service, servicehost=None, monitorrunningon=None):
-        'Add a monitoring rule if this matches our (compiled) regular expression'
-        if monitoringrunningon is None:
-            monitoringrunningon=servicehost
-        if servicehost is None or monitorrunningon is not servicehost:
-            return False
-        exename = service['exe']
-        if self.regex.match(exename) is not None:
-            self.createamonitoringrule(self.monclass, self.servicename)
-            return True
-        return False
-
-if __name__ == '__main__':
-    mon = MonitorAction('global', 'DummyName', 'OCF', 'Dummy', 1, 120, provider='heartbeat')
-    print mon.construct_mon_json()
-
+    pass
