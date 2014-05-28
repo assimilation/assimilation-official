e3cfcd361b57c8d35d52d1ee524450ce2b6cb36d
Alan Robertson <alanr@unix.sh>
2014-05-28 13:15:54 -0600

    Not sure why ldconfigs don't persist from one image to another - but they don't seem to...

--- docker/Ubuntu/post-build.sh
+++ docker/Ubuntu/post-build.sh
@@ -5,5 +5,6 @@ wget -q http://dist.neo4j.org/neo4j-community-2.0.1-unix.tar.gz -O /tmp/neo4j-co
 sleep 30
 kill $!
 /etc/init.d/neo4j-service status
+ldconfig /usr/lib/x86_60-linux-gnu/assimilation
 cd /root/assimilation/src
 testify -v cma.tests
