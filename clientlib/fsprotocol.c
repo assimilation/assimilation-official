6ace5eb6d13d9b7ba690a9152d11c421a9c0b2c7
Alan Robertson <alanr@unix.sh>
2014-08-19 01:45:05 -0600

    Changed shutdown sequence in FSA

--- clientlib/fsprotocol.c
+++ clientlib/fsprotocol.c
@@ -120,7 +120,7 @@ static const FsProtoState nextstates[FSPR_INVALID][FSPROTO_INVAL] = {
 
 static const unsigned actions[FSPR_INVALID][FSPROTO_INVAL] = {
 //	 START REQSEND GOTACK GOTCONN_NAK REQSHUTDOWN RCVSHUTDOWN       ACKTIMEOUT             OUTDONE  SHUT_TO
-/*NONE*/ {0,	0,      A_OOPS, A_CLOSE,  0,         A_ACKME|A_SNDSHUT, A_ACKTO|A_OOPS,        A_OOPS,  A_OOPS},
+/*NONE*/ {0,	0,      A_OOPS, A_CLOSE,  0,         A_ACKME|A_CLOSE, A_ACKTO|A_OOPS,        A_OOPS,  A_OOPS},
 /*INIT*/ {0,	0,	0,      A_CLOSE,  A_CLOSE,   A_ACKME|A_SNDSHUT, A_ACKTO|A_CLOSE,       0,       A_OOPS},
 /*UP*/   {0,	0,	0,      A_CLOSE,  A_SNDSHUT, A_ACKME|A_SNDSHUT, A_ACKTO,               0,       A_OOPS},
 // SHUT1: no ACK, no CONNSHUT 
