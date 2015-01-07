88af3673ffe331e501a1f6e932bfdb88ab4e3f49
Alan Robertson <alanr@unix.sh>
2015-01-06 23:32:05 -0700

    Fixed a pylint indentation complaint.

--- cma/frameinfo.py
+++ cma/frameinfo.py
@@ -485,7 +485,7 @@ class FrameSetTypes(object):
             #f.write('        {FRAMESETTYPE_%s,\t/*%d*/ %s},	\\\n' % (tup[0], i, Cobjname))
         #f.write('}\n')
 
-	f.write('#define MIN_SEQFRAMESET\tFRAMESETTYPE_ACK\t///<'
+        f.write('#define MIN_SEQFRAMESET\tFRAMESETTYPE_ACK\t///<'
 		' First frameset type with a sequence number\n')
         f.write('#endif /* _FRAMESETTYPES_H */\n')
 
