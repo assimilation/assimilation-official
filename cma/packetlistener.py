b0484b8a1426a8cce4fd82a5a6646b0cdfb2b003
Alan Robertson <alanr@unix.sh>
2015-01-31 00:09:33 -0700

    Attempts to fix the persistent GIOFunc() result not being persistent... And some minor pylint changes.

--- cma/packetlistener.py
+++ cma/packetlistener.py
@@ -94,7 +94,8 @@ class PacketListener(object):
         self.dispatcher = dispatch
         self.source = None
         self.mainloop = glib.MainLoop()
-        #print >> sys.stderr, ('self.mainloop %s, self.mainloop.mainloop: %s' % (self.mainloop, self.mainloop.mainloop))
+        #print >> sys.stderr, ('self.mainloop %s, self.mainloop.mainloop: %s'
+        #   % (self.mainloop, self.mainloop.mainloop))
         # W0612: unused variable j
         # pylint: disable=W0612
         self.prio_queues = [[] for j in range(PacketListener.LOWEST_PRIO+1)]
@@ -213,7 +214,7 @@ class PacketListener(object):
                 cond = '(0x%08x??)' % (int(cb_condition))
             CMAdb.log.warning('mainloop_callback: Received Unexpected I/O condition: %s '
             %       (cond))
-            CMAdb.log.warning('mainloop_callback: Called with (%s, %s, %s)' 
+            CMAdb.log.warning('mainloop_callback: Called with (%s, %s, %s)'
             %   (unusedsource, cb_condition, listener))
             return True
         #print >> sys.stderr, 'RETURNING True'
