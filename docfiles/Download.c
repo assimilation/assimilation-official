064e5b31032746b54581e48103b688b0009cb199
Alan Robertson <alanr@unix.sh>
2015-03-06 14:32:05 -0700

    Added a download tab, link and icon for downloading the software from the AssimilationSystems web site.

---
+++ docfiles/Download.c
@@ -0,0 +1,10 @@
+/**
+@page download Download Prebuilt Assimilation Project Packages
+@htmlonly<a href="http://assimilationsystems.com/download/"><img alt="download button" src="http://assimilationsystems.com/wp-content/uploads/2015/03/Download-Doxygen.png"</img></a>@endhtmlonly
+
+Click on the <a href="http://assimilationsystems.com/download/">download</a> 
+button to get connected to the Assimilation community, receive links to prebuilt packages along with installation and usage tips.
+
+If you would prefer to build your own packages from source, check out the @ref GettingStarted section in the project web site.
+
+*/
