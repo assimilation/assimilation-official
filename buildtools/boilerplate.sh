06d1453fba323957ddf6df7705435da0d1a4b21d
Alan Robertson <alanr@unix.sh>
2015-09-25 23:59:40 -0600

    Minor changes to the boilerplate shell/python file
    Shorten lines, that kind of thing.

--- buildtools/boilerplate.sh
+++ buildtools/boilerplate.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
-#!/bin/bash -eu
-# vim: smartindent tabstop=4 shiftwidth=4 expandtab number
+#!/bin/sh -eu
+#
+# vim: smartindent tabstop=4 shiftwidth=4 expandtab number colorcolumn=100
 #
 # This file is part of the Assimilation Project.
 #
 # Author: Alan Robertson <alanr@unix.sh>
 # Copyright (C) 2015 - Assimilation Systems Limited
 #
-# Free support is available from the Assimilation Project community - http://assimproj.org
-# Paid support is available from Assimilation Systems Limited - http://assimilationsystems.com
+# Free support is available from the Assimilation Project community
+#   - http://assimproj.org
+# Paid support is available from Assimilation Systems Limited
+#   - http://assimilationsystems.com
 #
-# The Assimilation software is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# The Assimilation software is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
@@ -21,6 +24,6 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with the Assimilation Project software.  If not, see http://www.gnu.org/licenses/
-#
+# along with the Assimilation Project software.
+# If not, see http://www.gnu.org/licenses/
 #
