e12babaec7614b7c0355f704227ac57f8e0660ea
borgified <borgified@gmail.com>
2020-08-17 02:46:51 -0700

    add attach_asset.sh helper script

---
+++ ci/scripts/attach_asset.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eu
+
+# this script attaches a file to a particular release as an asset
+# usage: ./attach_asset.sh <path> <file> <tag>
+
+# prereqs
+# run this script in $TRAVIS_BUILD_DIR (hub needs to run at the root of repo)
+
+PATH_TO_FILE=$1
+FILE=$2
+TAG=$3
+
+# first we need to check if the release at <tag> already has this asset. if so we have to delete it first, then we can upload.
+
+ASSETS=$(hub api repos/assimilation/assimilation-official/releases | jq -r '.[]|select(.tag_name=="$TAG")|.assets[]|select(.name=="$FILE")|[.name,.url]|@tsv')
+
+if [ "$ASSETS" == "" ]; then
+  hub release edit -a "$PATH_TO_FILE/$FILE" -m "fake release to test attaching binary" -m "test" $TAG
+else
+  # hub api -X DELETE $(echo $ASSETS | awk '{print $2}'
+  echo $ASSETS | awk '{print $2}'
+  # hub release edit -a "$PATH_TO_FILE/$FILE" -m "fake release to test attaching binary" -m "test" $TAG
+fi
