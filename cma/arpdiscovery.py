55b30a2dc32b6042a237874205f6b76c6c97ae91
Alan Robertson <alanr@unix.sh>
2020-09-09 22:08:16 -0600

    Fixed a typo.

--- cma/arpdiscovery.py
+++ cma/arpdiscovery.py
@@ -182,7 +182,7 @@ class ArpDiscoveryListener(DiscoveryListener):
         device_name = jsonobj["device"]
         device = drone.find_nic(device_name)
         if device is None:
-            print(f"********************Cannot find NIC '{device_name}' for drone {drone}"))
+            print(f"********************Cannot find NIC '{device_name}' for drone {drone}")
             return
         mac_ip_table = {}
         # Group the IP addresses by MAC address - inverting the map
